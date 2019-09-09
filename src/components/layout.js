410bdcc181df606e148fec578318af649b7b8504
jonniebigodes <jonniebigodes@hotmail.com>
2019-09-09 21:49:10 +0100

    no more func comp

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,29 +5,27 @@ import Counter from "./Counter"
 
 /* const DefaultLayout = props => {
   const { children } = props */
-const DefaultLayout = ({children}) => {
-  return (
-    <div>
-      <Link to="/">
-        <h3>MobX example</h3>
-      </Link>
-      <Counter />
+const DefaultLayout = ({children}) => (
+  <div>
+    <Link to="/">
+      <h3>MobX example</h3>
+    </Link>
+    <Counter />
 
-      <ul>
-        <li>
-          <Link to="/a/">a</Link>
-        </li>
-        <li>
-          <Link to="/b/">b</Link>
-        </li>
-        <li>
-          <Link to="/c/">c</Link>
-        </li>
-      </ul>
-      {children}
-    </div>
-  )
-}
+    <ul>
+      <li>
+        <Link to="/a/">a</Link>
+      </li>
+      <li>
+        <Link to="/b/">b</Link>
+      </li>
+      <li>
+        <Link to="/c/">c</Link>
+      </li>
+    </ul>
+    {children}
+  </div>
+)
 DefaultLayout.propTypes = {
   children: PropTypes.object.isRequired
 }
